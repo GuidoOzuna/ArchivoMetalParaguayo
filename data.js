3060f0c39a3b3658c4dc86f9c3a2bbdf996ca57f
GuidoOzuna <guido-.com@outlook.com>
2025-05-29 11:57:20 -0300

    Mensaje de actualización

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const archivos = [
     {
-        titulo: "Sendero al olvido",
+        titulo: "Patea",
         portada: "img/220_voltios/patea.jpg",
         banda: "220 Voltios",
         genero: "Heavy Metal",
@@ -10,22 +10,126 @@ const archivos = [
         lanzamiento: "2008",
         discografica: "Independiente",
         miembros: [
-            "Juan Vera",
-            "Horacio Careaga",
-            "Richard Ariasx",
-            "David Cano",
-            "Javier Isasi"
+            "Itto Rivas",
+            "Camilo Ramírez",
+            "Mario Garcia",
+            "Nacho Galeano",
+            "Carlos Carvallo"
         ],
         canciones: [
             "01- Rockero Impostor",
             "02- Donde rock y alcohol",
-            "03- Montado en lanmuertr",
+            "03- Montado en la muerte",
             "04- Carretera del rock",
             "05- AC/DC (220 Voltios)",
             "06- Riff, cueros y tachas"
         ],
         redes: [
-            { platform: "Bandcamp", url: "https://anhellical.bandcamp.com" }
+            { platform: "Bandcamp", url: "N/A" }
+          ]
+    },
+        {
+        titulo: "Descargando Rock!!!",
+        portada: "img/220_voltios/descargando_rock!!!.jpg",
+        banda: "220 Voltios",
+        genero: "Heavy Metal",
+        pais: "Paraguay",
+        tipo: "Ep",
+        formato: "Cd",
+        lanzamiento: "2008",
+        discografica: "Independiente",
+        miembros: [
+            "Itto Rivas",
+            "Camilo Ramírez",
+            "Mario Garcia",
+            "Nacho Galeano",
+            "Federico Vera"
+        ],
+        canciones: [
+            "01- Patea",
+            "02- Rockero Impostor",
+            "03- Donde rock y alcohol",
+            "04- Días de gloria",
+            "05- Montando en la muerte",
+            "06- Heavy Metal",
+            "07- AC-DC",
+            "08- Carretera del Rock",
+            "09- Riff, cueros y tachas"
+        ],
+        redes: [
+            { platform: "Bandcamp", url: "N/A" }
+          ]
+    },
+        {
+        titulo: "En vivo - Absoluto Rock",
+        portada: "img/220_voltios/en_vivo_-_absoluto_rock.jpg",
+        banda: "220 Voltios",
+        genero: "Heavy Metal",
+        pais: "Paraguay",
+        tipo: "Ep",
+        formato: "Cd",
+        lanzamiento: "2009",
+        discografica: "Independiente",
+        miembros: [
+            "Itto Rivas",
+            "Camilo Ramírez",
+            "Mario Garcia",
+            "Nacho Galeano",
+            "Esteban Centurion"
+        ],
+        canciones: [
+            "01- Patea",
+            "02- Rockero impostor",
+            "03- Noche de Rock y alcohol",
+            "04- Las cuerdas del Diablo",
+            "05- Montado en la muerte",
+            "06- La espada sagrada (Riff cover)",
+            "07- Carretera del Rock (El camino marchito)",
+            "08- Días del gloria (Rock and Roll sin parar)",
+            "09- AC/DC",
+            "10- Riff, cuero y tachas",
+            "11- Breakthoven (Barón Rojo cover)",
+            "12- Heavy Metal",
+            "13- Destrucción (V8 cover)"
+        ],
+        redes: [
+            { platform: "Bandcamp", url: "N/A" }
+          ]
+    },
+            {
+        titulo: "220 Voltios",
+        portada: "img/220_voltios/220_voltios.jpg",
+        banda: "220 Voltios",
+        genero: "Heavy Metal",
+        pais: "Paraguay",
+        tipo: "Álbum",
+        formato: "Cd",
+        lanzamiento: "2009",
+        discografica: "Las Palmeras Records",
+        miembros: [
+            "Itto Rivas",
+            "Camilo Ramírez",
+            "Mario Garcia",
+            "Nacho Galeano",
+            "Esteban Centurion"
+        ],
+        canciones: [
+            "01- Patea",
+            "02- Heavy Metal",
+            "03- Nada me detendrá",
+            "04- Noche de rock y alcohol",
+            "05- Las cuerdas del diablo",
+            "06- AC/DC",
+            "07- Días de gloria",
+            "08- Rockero impostor",
+            "09- Montando en la muerte",
+            "10- Riff, cuero y tachas",
+            "11- La carretera del rock",
+            "12- La espada sagrada (Riff cover) ",
+            "13- Destrucción (V8 cover) (live)"
+        ],
+        redes: [
+            { platform: "Bandcamp", url: "N/A" }
           ]
     }
 ];
